b109dc228df27515a5c3828eb72b20c3d0f8459f
jaumemy <jaume.montane@outlook.com>
2021-02-19 12:43:01 -0500

    Part 12 - Using default db isntead of Heroku's Postgresql

--- locallibrary/settings.py
+++ locallibrary/settings.py
@@ -137,7 +137,7 @@ EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 # Heroku: Update database configuration from $DATABASE_URL.
 import dj_database_url
 db_from_env = dj_database_url.config(conn_max_age=500)
-DATABASES['default'].update(db_from_env)
+# DATABASES['default'].update(db_from_env)
 
 
 # The absolute path to the directory where collectstatic will collect static files for deployment.
